1bab694083975c9f5a1ab6b9acfa78f9d37a79c0
Andrii Hermak <hermak20102004@gmail.com>
2022-12-30 15:02:48 +0100

    #1. Add main file and settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """ Клас для збереження всіх налаштувань гри """
+
+    def __init__(self):
+        """ Ініціалізація налаштувань гри """
+        # Параметри екрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+
